539f64cc736ace82cbcf7a7db0b8348a191cbfa3
Marty Spellerberg <marty@martyspellerberg.com>
2014-06-09 18:11:39 -0500

    Strip-out non-trustees (more)

---
+++ footer.php
@@ -0,0 +1,50 @@
+<div class="footer">
+
+	<div class="column first">
+
+		<h2>The Heckscher Foundation for Children</h2>
+
+		<div class="social">
+			<ul>
+				<li><a href="https://www.facebook.com/HeckscherFoundation" class="icon icon-facebook"><span>Facebook</span></a></li>
+				<li><a href="https://twitter.com/HeckscherFnd" class="icon icon-twitter"><span>Twitter</span></a></li>
+				<li><a href="http://instagram.com/heckscherfoundation" class="icon icon-instagram"><span>Instagram</span></a></li>
+			</ul>
+		</div>
+
+	</div>
+
+	<div class="column">
+
+		<h3><a href="/about/contact-us/">Contact</a></h3>
+
+		<p class="contact"><span>123 East 70th Street</span>
+		<span>New York, NY 10021</span>
+		<span>212-744-0190 Tel</span>
+		<span>212-744-2761 Fax</span>
+		<span><a href="mailto:grants@heckscherfoundation.org">Email</a></span></p>
+
+	</div>
+
+	<div class="column">
+
+		<h3><a href="/info/">Additional Information</a></h3>
+
+		<ul>
+			<li><a href="http://www.etc-nyc.com/" target="_blank">Site by <span class="etc">ETC</span></a></li>
+		</ul>
+
+	</div>
+
+</div>
+
+<p class="copyright">&copy; <?php echo date('Y')?> The Heckscher Foundation for Children</p>
+
+<p class="toplink"><a href="#top" title="Scroll to top"><span class="icon icon-togglearrow-up"></span><span class="label">Scroll to top</span></a></p>
+
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
